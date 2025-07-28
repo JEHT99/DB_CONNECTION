f4a57caa89cffc1feb5047ef9fbf8f71fa3c0c21
Jeht99 <Jeht99@outlook.com>
2025-07-27 19:31:09 -0600

    [dbConnectionn.py]: VerifyConnection wrapper defined

--- dbConecction.py
+++ dbConecction.py
@@ -6,6 +6,20 @@ from dotenv import load_dotenv
 #------------------------------------------------------------------
 load_dotenv()
 #------------------------------------------------------------------
+def VerifyConnection(func):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        sql = "SELECT 1;"
+        try:
+            with (self.client).cursor() as cursor:
+                cursor.execute(sql)
+        except:
+            if self.client:
+                (self.client).close()
+            self.client = self._reconect()
+        return func(self, *args, **kwargs)
+    return wrapper
+#------------------------------------------------------------------
 class PostgreSQL():
     def __init__(self):
         self.client = self.__startConnection()
