0232579016e842c0cf80821e52ebf613d0ea4700
Jeht99 <Jeht99@outlook.com>
2025-07-27 19:24:41 -0600

    [dbConnection.py]]: PostgreSQL class created with constructor & destructor

---
+++ dbConecction.py
@@ -0,0 +1,29 @@
+import os
+import time
+import psycopg2
+from functools import wraps
+from dotenv import load_dotenv
+#------------------------------------------------------------------
+load_dotenv()
+#------------------------------------------------------------------
+class PostgreSQL():
+    def __init__(self):
+        self.client = self.__startConnection()
+    #--------------------------------------------------------------
+    def __del__(self):
+        if self.client:
+            (self.client).close()
+    #--------------------------------------------------------------
+    def __startConnection(self)-> (object|None):
+        try:
+            return psycopg2.connect(
+                            database = os.getenv("DATABASE"), 
+                            user = os.getenv("USER"), 
+                            host= os.getenv("HOST"),
+                            password = os.getenv("PASSWORD"),
+                            port = os.getenv("PORT")
+                            )
+        except Exception as e:
+            print(e)
+            return None
+#------------------------------------------------------------------
