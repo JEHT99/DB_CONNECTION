e1d62f8f59ca8fac9ee7d9110606b070d8078068
Jeht99 <Jeht99@outlook.com>
2025-12-25 09:32:57 -0700

    [sqLite.py]: SqLite class created

---
+++ sqLite.py
@@ -0,0 +1,25 @@
+import os
+import sqlite3
+from absDb import AbsDb
+from dotenv import load_dotenv
+#------------------------------------------------------------------
+load_dotenv()
+#------------------------------------------------------------------
+class SqLite(AbsDb):
+    def _startConnection(self)-> (object|None):
+        db = os.getenv("DATABASE")
+        if db is None:
+            return None
+        
+        if not db.endswith(".db"): 
+            return None
+        
+        if db not in os.listdir():
+            return None
+        
+        try:
+            return sqlite3.connect(db)
+        except Exception as e:
+            print(e)
+            return None
+#------------------------------------------------------------------
